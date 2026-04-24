afed5600d7e40d43d4901824546ef15c670abe84
Fatih Sarıoğlu <ftihsrglu@gmail.com>
2026-04-24 14:15:52 +0300

    lamport clock and tree implementation

--- CS_403-534_Project_Phase_1/src/tree_crdt/clock/lamport.py
+++ CS_403-534_Project_Phase_1/src/tree_crdt/clock/lamport.py
@@ -2,4 +2,27 @@ from . import Clock
 
 class LamportClock(Clock):
   # TODO: Implement this class by inheriting from Clock
-  pass
+  def __init__(self, id):
+    # The ID of the replica that owns this clock
+    self.__id = id
+    # Lamport timestamps start at 0
+    self.__timestamp = 0
+
+  @property
+  def id(self):
+    return self.__id
+
+  @property
+  def timestamp(self):
+    return self.__timestamp
+
+  def update(self, received):
+    if received is None:
+      self.__timestamp += 1
+    else:
+      self.__timestamp = max(self.__timestamp, received) + 1
+
+  def __str__(self):
+    return str(self.__timestamp)
+
+    
